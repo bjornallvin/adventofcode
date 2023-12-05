39dbbb7e745291beb946bdc4f716a001e5c89ac8
bjornallvin <bjorn@allvin.com>
2023-12-05 11:05:41 +0100

    2015 d2

---
+++ src/2015/d2/p2.ts
@@ -0,0 +1,23 @@
+import { readLinesFromFile } from '../../utils'
+
+let presents = readLinesFromFile(__dirname + '/data.txt')
+
+let sum = 0
+
+for (let pl of presents) {
+  let p = pl
+    .split('x')
+    .map((s) => parseInt(s))
+    .sort((a, b) => a - b)
+
+  //let sides = 2 * p[0] * p[1] + 2 * p[0] * p[2] + 2 * p[1] * p[2] + p[0] * p[1]
+
+  let sides = p[0] + p[0] + p[1] + p[1]
+  sides += p[0] * p[1] * p[2]
+
+  sum = sum + sides
+
+  console.log(p, sides)
+}
+
+console.log(sum)
